b3e8d9de106b654b9189afc0043f47d7bec012ab
lucas <lucasmaiaxgo@gmail.com>
2025-05-12 15:55:25 -0300

    terceiro filme (apps.js)

--- public/apps.js
+++ public/apps.js
@@ -17,4 +17,13 @@ const filmes = [
         descricaoCurta: "Um trio vai acampar numa floresta amaldiçoada para descobrir a verdade sobre uma lenda local."
         descricaoLonga: "Os filmes de terror e, principalmente, os filmes found footage, não seriam os mesmos sem esse filme. É sensacional, e eu nem gosto de filmes de terror, então posso atestar que esse é bom de verdade. Vejo pessoas dizendo que ele não é tão ""assustador"", no sentido literal da palavra, e devo dizer que concordo, mas não significa que o filme não dê medo, ele dá, e muito, só que a forma como o medo é construído aqui é muito diferente dos filmes de terror atuais... não tem jumpscare, é pura atmosfera, puro caos, pura incerteza. É como se esse filme fosse a personificação cinematográfica de uma música de black metal. "
     },
-
+    {
+        id: 3,
+        titulo: "The End of Evangelion",
+        imagem: "https://preview.redd.it/i29iqscbs2d41.jpg?auto=webp&s=f6f950699a96d4b08310368cf70f407492c74176",
+        autor: "Hideaki Anno e Kazuya Tsurumaki",
+        ano: "19 de julho de 1993 (no Japão)",
+        descricaoCurta: "Não tem descrição curta adequada pra esse filme.",
+        descricaoLonga: "É com certeza o mais viajado da lista, e chega a ser difícil descrevê-lo porque não é um filme que funciona bem por conta própria, é meio que necessário que você tenha assistido o anime antes, mas, em suma, é o final oficial de Neon Genesis Evangelion, é onde nós acompanhamos a destruição da NerV e o projeto de instrumentalização humana em si. É talvez o filme mais angustiante e mais comtemplativo que eu já vi, é muito bom mesmo; a cena da Asuka sendo morta merece ser assistida por todo mundo pelo menos uma vez na vida."
+    },
+    
