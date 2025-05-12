7385312ff7189ab4933caefe0853780705a032fe
lucas <lucasmaiaxgo@gmail.com>
2025-05-12 16:07:10 -0300

    virgulas no app.js
    tinha virgulas faltando na descricao curta de 2 filmes

--- public/apps.js
+++ public/apps.js
@@ -14,7 +14,7 @@ const filmes = [
         imagem: "https://m.media-amazon.com/images/M/MV5BNGFmYzA5NzktOTc1Ni00YTIxLWJiZTEtNmEyZjBjNDM2MjRlXkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg",
         autor: "Eduardo Sánchez e Daniel Myrick",
         ano: "1 de outubro de 1999 (no Brasil)",
-        descricaoCurta: "Um trio vai acampar numa floresta amaldiçoada para descobrir a verdade sobre uma lenda local."
+        descricaoCurta: "Um trio vai acampar numa floresta amaldiçoada para descobrir a verdade sobre uma lenda local.",
         descricaoLonga: "Os filmes de terror e, principalmente, os filmes found footage, não seriam os mesmos sem esse filme. É sensacional, e eu nem gosto de filmes de terror, então posso atestar que esse é bom de verdade. Vejo pessoas dizendo que ele não é tão ""assustador"", no sentido literal da palavra, e devo dizer que concordo, mas não significa que o filme não dê medo, ele dá, e muito, só que a forma como o medo é construído aqui é muito diferente dos filmes de terror atuais... não tem jumpscare, é pura atmosfera, puro caos, pura incerteza. É como se esse filme fosse a personificação cinematográfica de uma música de black metal. "
     },
     {
@@ -32,7 +32,7 @@ const filmes = [
         imagem: "https://upload.wikimedia.org/wikipedia/pt/8/8b/LA_HAINE.jpeg",
         autor: "Mathieu Kassovitz",
         ano: "1 de dezembro de 1995 (no Brasil)",
-        descricaoCurta: "Um dia na vida de um trio de amigos no meio de tensões políticas em Paris."
+        descricaoCurta: "Um dia na vida de um trio de amigos no meio de tensões políticas em Paris.",
         descricaoLonga: "É um dos filmes mais legais que eu já vi, legal no sentido literal da palavra. É um filme muito bem filmado, que utiliza enquadramentos muito interessantes, que tem um roteiro muito bom, atuações muito boas... é um filme muito bom no geral. A relação dos três personagens e o entorno deles é muito bem feita também, você sente o ódio dos personagens e sente a tensão ao redor deles."
     }
 ];
