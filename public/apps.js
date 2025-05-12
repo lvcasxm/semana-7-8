762bbfd78eb4b4820ada1d0aa2db90834f3c7e2f
lucas <lucasmaiaxgo@gmail.com>
2025-05-12 15:54:15 -0300

    adicionando os filmes (apps.js)
    tinha esquecido de ir commitando oq eu ia fazendo entao vou ir fazendo isso

---
+++ public/apps.js
@@ -0,0 +1,10 @@
+const filmes = [
+    {
+        id: 1,
+        titulo: "Twin Peaks: Fire Walk with Me",
+        imagem: "https://m.media-amazon.com/images/M/MV5BYzJmZGFiMzAtODNhOC00NzAwLWFlMzAtYTQzYmRmYjIwNDgyXkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg",
+        autor: "Dirigido por David Lynch e co-escrito por Robert Engels",
+        ano: "14 de maio de 1993 (no Brasil)",
+        descricaoCurta: "Prequela dos acontecimentos da série Twin Peaks.",
+        descricaoLonga: "Acompanhamos aqui os últimos dias de Laura Palmer e sua eventual morte. É um filme visceral que destrincha todos os aspectos da vida da personagem; seu namoro conturbado, seu caso com um colega de escola, o abuso sofrido em casa, o consumo de drogas, e a prostituição.",
+   
